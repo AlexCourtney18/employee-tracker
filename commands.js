53d489c21fcae2082bd355e05c5b8c86702df4d9
Alex <lex.courtney18@gmail.com>
2022-08-12 15:09:09 -0500

    finish console.table functionality

--- commands.js
+++ commands.js
@@ -1,17 +1,52 @@
+const mysql = require('mysql2');
+const cTable = require('console.table');
+// const { promptUser } = require('./index');
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Password',
+    database: 'company'
+});
 
 const viewDepartments = () => {
-    `SELECT FROM * departments;`
+    connection.execute(
+        `SELECT * FROM departments;`,
+        function(err, results) {
+            const table = cTable.getTable(results);
+            console.log(table);
+        }
+        );
 };
 
 const viewRoles = () => {
-    `SELECT FROM * roles;`
+    connection.execute(
+    `SELECT * FROM roles;`,
+    function(err, results) {
+        const table = cTable.getTable(results);
+        console.log(table);
+    }
+    );
 };
 
 const viewEmployees = () => {
-    `SELECT FROM * employees;`
+    connection.execute(
+    `SELECT * FROM employees;`,
+    function(err, results) {
+        const table = cTable.getTable(results);
+        console.log(table);
+    }
+    );
 };
 
 const addDepartment = () => {
-    
+
+}
+
+
+
+module.exports = {
+    viewDepartments,
+    viewRoles,
+    viewEmployees
 }
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const { viewDepartments, viewRoles, viewEmployees } = require('./commands');
 
 const promptUser = () => {
     console.log(`
@@ -12,8 +13,21 @@ const promptUser = () => {
             name: 'menu',
             message: "What would you like to do?",
             choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role'],
-        }
+        },
     ])
+        .then(choice => {
+            if (choice.menu === 'View All Departments') {
+                console.log('View All Departments');
+                viewDepartments();
+            } else if (choice.menu === 'View All Roles') {
+                console.log('View All Roles');
+                viewRoles();
+            } else if (choice.menu === 'View All Employees') {
+                console.log('View All Employees');
+                viewEmployees();
+            }
+        })
+        .then(promptUser);
 }
 
 promptUser()
