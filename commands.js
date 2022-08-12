3cb374e38a56e9e2a27710ea4c93264d063b1c58
Alex <lex.courtney18@gmail.com>
2022-08-12 13:04:42 -0500

    create classes and constructors for Department, Employee, and Role

---
+++ commands.js
@@ -0,0 +1,17 @@
+
+
+const viewDepartments = () => {
+    `SELECT FROM * departments;`
+};
+
+const viewRoles = () => {
+    `SELECT FROM * roles;`
+};
+
+const viewEmployees = () => {
+    `SELECT FROM * employees;`
+};
+
+const addDepartment = () => {
+    
+}
